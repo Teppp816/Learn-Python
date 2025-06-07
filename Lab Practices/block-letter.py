567fb71626086c90fd806c4e342b9a91d658247e
Stephanie <spayumo816@gmail.com>
2025-06-07 14:31:05 +0800

    Add files via upload

---
+++ Lab Practices/block-letter.py
@@ -0,0 +1,26 @@
+# Block Letters
+# ASCII art is a way of creating images using the individual characters from a keyboard.
+
+# For example, below is an ASCII art of some letters from the English alphabet.
+
+# In this project, we will write a Python program that displays the initials of your name in block characters to dip your toes into the world ASCII art.
+
+
+# Let’s start by writing two comments:
+
+# Your full name
+# Stephanie Payumo
+
+# A fun fact about yourself
+# I love learning new skills
+
+# Now that we have our name down, let’s output our first initial!
+# Try printing out the first character of your first or given name as a block character!
+
+print(" SSS    PPPP")
+print("S    S  P    P")
+print("S       P    P")
+print(" SSS    PPPP")
+print("     S  P")
+print("S    S  P")
+print(" SSS    P")
